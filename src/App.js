cf32febfa1450e9cdc7bf5e644feb95b580f2f9d
VartanovMaks <vartanov2009@gmail.com>
2021-03-30 14:11:39 +0300

    с фетчем и таймаутом

--- src/App.js
+++ src/App.js
@@ -1,109 +1,46 @@
-import React, {Component, useState} from 'react';
+import React, {Component, useEffect, useState} from 'react';
+const url='https://jsonplaceholder.typicode.com/todos'
 
-let interval;
-
-const Comp =()=>{
-  React.useEffect(()=>{
-    console.log('child componentDidMount')
-    interval = setInterval( ()=>{
-    console.log('fetching daTA')
-    },2000)
-    return ()=> {
-      console.log('child willUnMount')
-      clearInterval(interval)
-    }  
-  },[])
-  
-  // React.useEffect(()=>{
-  //   return ()=>console.log('child willUnMount')
-  // },[])
-  
-
-  return (
-    <h1>
-    Component to remove
-  </h1>    
-  )
-}
 const App =()=>{
   const[counter, setCounter] = useState(0);
+  const[todos, setTodos] = useState([]);
+  const[isLoading, setIsLoading] = useState(false);
   
   const incCounter = ()=> {
     setCounter(counter+1)
   }
 
-  React.useEffect(()=>{         // did mount
-    console.log('parent didMount')
-  },[])
+  const fetchTodos = async ()=>{
+    setIsLoading(true)
+    const respose = await fetch(url)
+    const data = await respose.json()
+    console.log(data)
+    setTodos(data)
+    //setTimeout(()=>{
+      setIsLoading(false)
+    //}, 1000)
+  }
   
-  React.useEffect(()=>{         // did update
-    console.log('parent didUpdate')
-    return ()=>{
-      console.log('parent willUnMount')
-    }
-  },[counter])
-  
+  useEffect( ()=>{
+    fetchTodos()
+  }, [])
+
+
   return (
     <>
         <h1 onClick={incCounter}> Hello react {counter} </h1>
-         {!!(counter % 2) && <Comp />} 
+        { !todos.length && isLoading &&(
+            <h1>LOADING DATA....</h1>
+        )}
+        {!!todos.length && (
+        <>
+          <hr />
+          <h3>{todos[0].title} - {todos[0].completed.toString()} - id:{todos[0].id} </h3>
+          <hr />
+        </>
+        )}
     </>
   )
 }
 
-// class Comp extends Component {
-  
-//   componentDidMount() {
-//     console.log('child componentDidMount')
-//     interval = setInterval( ()=>{
-//       console.log('fetching daTA')
-//     },2000)
-//   }
-  
-//   componentWillUnmount() {
-//     console.log('child componentWillUnmount')
-//     clearInterval(interval)
-    
-//   }
-//   render() {
-//     console.log('child render')
-//     return (
-//       <h1>
-//         Component to remove
-//       </h1>
-//     );
-//   }
-// }
-
-// class App extends Component {
-// state = {
-//   counter:0
-// }
-
-// componentDidMount() {
-//   console.log('componentDidMount')
-// }
-
-// componentDidUpdate(prevProps, prevState) {
-//   console.log('componentDidUpdate',{prevProps, prevState})
-  
-// }
-
-
-// incCounter = ()=>{
-//   this.setState({counter:this.state.counter+1})
-// }
-//   render() {
-   
-//     console.log('render')
-//     {this.state.counter>5 && this.setState({counter:0})}
-//     return (
-//       <div>
-//         <h1 onClick={this.incCounter}> Hello react {this.state.counter} </h1>
-//          {/* !!this.state.counter - приведение к булевому типу!! */}
-//         {!!(this.state.counter % 2) && <Comp />}
-//       </div>
-//     );
-//   }
-// }
 export default App;
